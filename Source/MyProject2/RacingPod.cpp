ecb8e431ad592a85f79b50626c374842122925df
Gabriel Medoukali <gabriel.medoukali@epitech.eu>
2024-01-19 19:08:02 +0100

    feat : racing pods & code

--- Source/MyProject2/RacingPod.cpp
+++ Source/MyProject2/RacingPod.cpp
@@ -10,7 +10,6 @@
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
 
-
 // Sets default values
 ARacingPod::ARacingPod()
 {
@@ -19,8 +18,8 @@ ARacingPod::ARacingPod()
 
     // CAPSULE
     Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
-    Capsule->SetCapsuleHalfHeight(20.f);
-    Capsule->SetCapsuleRadius(15.f);
+    Capsule->SetCapsuleHalfHeight(1000.f);
+    Capsule->SetCapsuleRadius(1000.f);
     SetRootComponent(Capsule);
 
     // SPRING ARM
@@ -34,36 +33,14 @@ ARacingPod::ARacingPod()
     // SKELETAL MESH
     RacingPodMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("RacingPodMesh"));
     RacingPodMesh->SetupAttachment(GetRootComponent());
+    
+    // PAWN MOVEMENT
+    m_movementComponent = CreateDefaultSubobject<UFloatingPawnMovement>("RacingPodMovement");
+    m_movementComponent->MaxSpeed = RegularSpeed;
+    m_currentSpeed = MinSpeed;
+    m_currentRotationSpeed = MaxRotationSpeed;
 
     AutoPossessPlayer = EAutoReceiveInput::Player0;
-}
-
-void ARacingPod::Move(const FInputActionValue& Value)
-{
-    const float CurrentValue = Value.Get<float>();
-    
-    if (GEngine)
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Hello world %i"), CurrentValue));
-    }
-
-    if (Controller && (CurrentValue != 0.f))
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Should Move"));
-        FVector Forward = GetActorForwardVector();
-        AddMovementInput(Forward, CurrentValue);
-    }
-}
-
-void ARacingPod::Look(const FInputActionValue& Value)
-{
-    const FVector2D LookAxisValue = Value.Get<FVector2D>();
-
-    if (GetController())
-    {
-        AddControllerYawInput(LookAxisValue.X);
-        AddControllerPitchInput(LookAxisValue.Y);
-    }
 }
 
 // Called when the game starts or when spawned
@@ -78,16 +55,125 @@ void ARacingPod::BeginPlay()
             Subsystem->AddMappingContext(RacingPodMappingContext, 0);
         }
     }
-
-	
 }
 
 // Called every frame
 void ARacingPod::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
+    if (Controller)
+    {
+        //HandleGravity();
+        HandleAcceleration();
+    }
 }
+
+void ARacingPod::HandleGravity()
+{
+    FVector Up = GetActorUpVector();
+    AddMovementInput(Up, -1.0f * GravityScale);
+}
+
+void ARacingPod::HandleAcceleration()
+{
+    //if (GEngine) { GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("%i"), m_currentSpeed)); }
+    FVector Forward = GetActorForwardVector();
+
+    m_movementComponent->MaxSpeed = m_currentSpeed;
+    //soucis -> je add une value
+    AddMovementInput(Forward, m_currentSpeed);
+}
+
+void ARacingPod::Accelerate(const FInputActionValue& Value)
+{
+    const float CurrentValue = Value.Get<float>();
+
+    if (!Controller)
+    {
+        return;
+    }
+    if (CurrentValue != 0.f)
+    {
+        if (CurrentValue > 0)
+        {
+            m_currentRotationSpeed = MinRotationSpeed;
+            m_currentSpeed = MaxSpeed;
+        }
+        else
+        {
+            m_currentRotationSpeed = MaxRotationSpeed;
+            m_currentSpeed = MinSpeed;
+        }
+    }
+}
+
+void ARacingPod::CancelAccelerate()
+{
+    if (GEngine)
+    {
+        //GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("NOT ACCEL")));
+    }
+}
+
+void ARacingPod::Rotate(const FInputActionValue& Value)
+{
+    const float CurrentValue = Value.Get<float>();
+
+    if (Controller && (CurrentValue != 0.f))
+    {
+        FRotator rotationPod = FRotator(0.f, CurrentValue * m_currentRotationSpeed, 0.f);
+        AddActorWorldRotation(rotationPod);
+
+        //rotation between 70 -> -70 
+        // @todo : calculate the max Pitch from accel : AccelMax -> PitchMax
+        if (RacingPodMesh->GetComponentRotation().Pitch > 70 && CurrentValue > 0)
+        {
+            // DO NOTHING
+        }
+        else if (RacingPodMesh->GetComponentRotation().Pitch < -70 && CurrentValue < 0)
+        {
+            // DO NOTHING
+        }
+        else
+        {
+            RacingPodMesh->USceneComponent::AddLocalRotation(FRotator(CurrentValue, 0.f, 0.f));
+        }
+    }
+}
+
+void ARacingPod::CancelRotate()
+{
+    const float FactorRotation = 1 * ResetRotationSpeed;
+
+    if (int(abs(RacingPodMesh->GetComponentRotation().Pitch)) == 0)
+    {
+        return;
+    }
+    else if (RacingPodMesh->GetComponentRotation().Pitch > 0.f)
+    {
+        RacingPodMesh->USceneComponent::AddLocalRotation(FRotator(-FactorRotation, 0.f, 0.f));
+    }
+    else if (RacingPodMesh->GetComponentRotation().Pitch < 0.f)
+    {
+        RacingPodMesh->USceneComponent::AddLocalRotation(FRotator(FactorRotation, 0.f, 0.f));
+    }
+}
+
+/*void ARacingPod::Look(const FInputActionValue& Value)
+{
+    const FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+    // if (GEngine)
+    // {
+    //     GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Hello world %i"), LookAxisValue.Y));
+    // }
+
+    if (GetController())
+    {
+        AddControllerYawInput(LookAxisValue.X);
+        AddControllerPitchInput(LookAxisValue.Y * (- 1));
+    }
+}*/
 
 // Called to bind functionality to input
 void ARacingPod::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -96,8 +182,11 @@ void ARacingPod::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 
     if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
     {
-        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ARacingPod::Move);
-        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ARacingPod::Look);
+        EnhancedInputComponent->BindAction(AccelerateAction, ETriggerEvent::Triggered, this, &ARacingPod::Accelerate);
+        EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered, this, &ARacingPod::Rotate);
+
+        EnhancedInputComponent->BindAction(AccelerateAction, ETriggerEvent::None, this, &ARacingPod::CancelAccelerate);
+        EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::None, this, &ARacingPod::CancelRotate);
     }
 }
 
--- Source/MyProject2/RacingPod.h
+++ Source/MyProject2/RacingPod.h
@@ -6,8 +6,9 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "InputActionValue.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
 #include "RacingPod.generated.h"
-//#include "C:\Users\Gabriel\source\repos\MyProject2\Intermediate\Build\Win64\UnrealEditor\Inc\MyProject2\UHT\RacingPod.generated.h"
 
 class UCapsuleComponent;
 class USpringArmComponent;
@@ -31,17 +32,57 @@ protected:
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
+    void Accelerate(const FInputActionValue& Value);
+    void Rotate(const FInputActionValue& Value);
+    void CancelAccelerate();
+    void CancelRotate();
+
+    void HandleGravity();
+    void HandleAcceleration();
+
+    // Inputs
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
     UInputMappingContext* RacingPodMappingContext;
-
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
-    UInputAction* MoveAction;
-
+    UInputAction* AccelerateAction;
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
-    UInputAction* LookAction;
+    UInputAction* RotateAction;
 
-    void Move(const FInputActionValue& Value);
-    void Look(const FInputActionValue& Value);
+    // Move Speed
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxSpeed = 50000;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float RegularSpeed = 5000;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MinSpeed = 1000;
+    float m_currentSpeed = MinSpeed;
+
+    // Rotation Speed
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxRotationSpeed = 1;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float RegularRotationSpeed = 0.5;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MinRotationSpeed = 0.25;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float ResetRotationSpeed = 1;
+    float m_currentRotationSpeed;
+
+    // Rotation Properties
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SprintMaxRotationPitch = 70;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SlowMaxRotationPitch = 10;
+
+    // Acceleration / S
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Acceleration = 10;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float GravityScale = 50;
+
+    UFloatingPawnMovement* m_movementComponent;
+
 
 private:
     UPROPERTY(VisibleAnywhere)
